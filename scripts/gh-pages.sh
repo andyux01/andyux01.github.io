7bf0675fd2a014ebde32a0feb701149d95537f3e
Pesanweb Id <andy.yuli@gmail.com>
2025-02-05 14:40:11 +0700

    hasil export

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -5,7 +5,7 @@ set -e
 vendor/drush/drush/drush cron -l https://andy.propertysyariah.biz.id
 vendor/drush/drush/drush tome:static -y -l https://andy.propertysyariah.biz.id
 rm -rf gh-pages
-git clone git@github.com:andyux01/andyux01.github.io.git gh-pages
+# git clone git@github.com:andyux01/andyux01.github.io.git gh-pages
 cd gh-pages
 git checkout master || git checkout -b master
 cd ..
